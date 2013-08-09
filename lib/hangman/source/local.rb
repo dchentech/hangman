919fc1401341b84f56f427bd305b6f6f889afdd1
David Chen <mvjome@gmail.com>
2013-08-09 16:32:29 +0800

    Add is_not_desc_remain_only_already to Hangman::Local

--- lib/hangman/source/local.rb
+++ lib/hangman/source/local.rb
@@ -24,9 +24,15 @@ class Hangman::Local < Hangman::Source
   end
   
   def make_a_guess char
-    @remain_time -= 1 if @remain_time > 0
+    is_not_desc_remain_only_already = true
     @word.chars.to_a.each_with_index do |_c, idx|
-      @marked_word_hash[idx] = _c if char == _c
+      if char == _c
+        if (@remain_time > 0) && is_not_desc_remain_only_already
+          @remain_time -= 1 
+          is_not_desc_remain_only_already = false
+        end
+        @marked_word_hash[idx] = _c 
+      end
     end
 
     @current_response = {
